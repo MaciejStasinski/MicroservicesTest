2178bd90d354a4a31c0ace46e3d5bdd552ba6b30
Maciej Stasinski <m.stasinski1@gmail.com>
2021-12-18 10:51:31 +0100

    propertiestest

--- src/main/java/com/example/microservicestest/MicroservicesTestApplication.java
+++ src/main/java/com/example/microservicestest/MicroservicesTestApplication.java
@@ -1,13 +1,24 @@
 package com.example.microservicestest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicroservicesTestApplication {
 
+    @Autowired
+    private CarProperties carProperties;
+
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesTestApplication.class, args);
     }
+
+    @Bean
+    public void show(){
+        System.out.println(carProperties.mark + " "+ carProperties.getModel());
+    }
+
 
 }
