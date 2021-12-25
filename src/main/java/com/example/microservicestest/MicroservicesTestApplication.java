bdf2ce27205fb7b193500cecb115dd9628d75986
Maciej Stasinski <m.stasinski1@gmail.com>
2021-12-25 10:30:47 +0100

    commit

--- src/main/java/com/example/microservicestest/MicroservicesTestApplication.java
+++ src/main/java/com/example/microservicestest/MicroservicesTestApplication.java
@@ -3,7 +3,7 @@ package com.example.microservicestest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class MicroservicesTestApplication {
@@ -15,10 +15,9 @@ public class MicroservicesTestApplication {
         SpringApplication.run(MicroservicesTestApplication.class, args);
     }
 
-    @Bean
-    public void show(){
-        System.out.println(carProperties.mark + " "+ carProperties.getModel());
-    }
+
+
+
 
 
 }
