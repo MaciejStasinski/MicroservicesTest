323498e2ea5c9800c32ed46b47547a5c5aadba07
Maciej Stasinski <m.stasinski1@gmail.com>
2021-12-25 11:03:27 +0100

    commit

--- src/main/java/com/example/microservicestest/MicroservicesTestApplication.java
+++ src/main/java/com/example/microservicestest/MicroservicesTestApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroservicesTestApplication {
 
-    @Autowired
-    private CarProperties carProperties;
-
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesTestApplication.class, args);
     }
